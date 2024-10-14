4bc6115c4bccfc6ea23664f0c3a78523bf89291a
surachai-p <surachai.pi@kmitl.ac.th>
2024-10-15 00:58:24 +0700

    Add basic To-do List functionality and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('adds a todo when clicking the Add Todo button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const input = screen.getByPlaceholderText('Enter a todo');
+  const button = screen.getByText('Add Todo');
+
+  fireEvent.change(input, { target: { value: 'New todo item' } });
+  fireEvent.click(button);
+
+  expect(screen.getByText('New todo item')).toBeInTheDocument();
 });
